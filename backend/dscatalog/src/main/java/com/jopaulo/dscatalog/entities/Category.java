6c3ba54b41ea4c79ee1369b656db6dcfa986056f
jopaulo666 <jp_cbc@hotmail.com>
2021-08-17 23:55:58 -0300

    ORM mapping, database seeding

--- backend/dscatalog/src/main/java/com/jopaulo/dscatalog/entities/Category.java
+++ backend/dscatalog/src/main/java/com/jopaulo/dscatalog/entities/Category.java
@@ -2,12 +2,15 @@ package com.jopaulo.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -28,6 +31,9 @@ public class Category implements Serializable {
 
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updateAt;
+
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 
 	public Category() {
 	}
@@ -68,14 +74,19 @@ public class Category implements Serializable {
 	public Instant getUpdateAt() {
 		return updateAt;
 	}
-	
+
 	@PrePersist
 	public void prePersist() {
 		createdAt = Instant.now();
 	}
+
 	@PreUpdate
 	public void preUpdate() {
 		updateAt = Instant.now();
+	}
+
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@Override
