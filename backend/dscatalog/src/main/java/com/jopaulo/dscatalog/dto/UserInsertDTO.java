c7fc206405a525c6860f557064485ff8c9d9dfe8
jopaulo666 <jp_cbc@hotmail.com>
2021-09-15 00:05:22 -0300

    Custom ConstrantValidation

--- backend/dscatalog/src/main/java/com/jopaulo/dscatalog/dto/UserInsertDTO.java
+++ backend/dscatalog/src/main/java/com/jopaulo/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.jopaulo.dscatalog.dto;
 
+import com.jopaulo.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO{
 	private static final long serialVersionUID = 1L;
 	
--- backend/dscatalog/src/main/java/com/jopaulo/dscatalog/repositories/UserRepository.java
+++ backend/dscatalog/src/main/java/com/jopaulo/dscatalog/repositories/UserRepository.java
@@ -3,10 +3,10 @@ package com.jopaulo.dscatalog.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.jopaulo.dscatalog.entities.Product;
 import com.jopaulo.dscatalog.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	User findByEmail(String email);
 }
