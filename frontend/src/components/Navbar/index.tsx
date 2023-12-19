293352b188a31756a6b2f5d52268a8b3f67368b7
João Paulo <jp_cbc@hotmail.com>
2023-12-19 20:46:29 -0300

    menu hambuerguer

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,12 +1,29 @@
+import './styles.css'
+import '@popperjs/core';
+import 'bootstrap/js/src/collapse';
+
 const Navbar = () => {
     return (
-        <nav className="bg-primary">
-            <div>
+        <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
+            <div className="container-fluid">
                 <a href="link">
                     <h4>DS Catalog</h4>
                 </a>
-                <div>
-                    <ul>
+
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#dscatalog-navbar"
+                    aria-controls="dscatalog-navbar"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-toggler" id="dscatalog-navbar">
+                    <ul className="navbar-nav offset-md-2 main-menu">
                         <li>
                             <a href="link">HOME</a>
                         </li>
