d7350f45e72a0262449831d1975b46745da68709
João Paulo <jp_cbc@hotmail.com>
2023-12-19 21:14:55 -0300

    Página Home - estrutura básica do HTML

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,11 @@
-import Navbar from 'components/Navbar';
 import './assets/styles/custom.scss';
 import './App.css';
+import Home from 'pages/Home';
 
 const App = () => {
 
   return (
-    <>
-      <Navbar />
-      <h1>Aló mundo cruel</h1>
-    </>
+    <Home />
   );
 }
 
