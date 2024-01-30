0f4e368e963c892bd133de99f109123e5ff40881
João Paulo <jp_cbc@hotmail.com>
2024-01-29 22:53:51 -0300

    Redirecionando para a rota pretendida após o login

--- frontend/src/pages/Admin/Auth/Login/index.tsx
+++ frontend/src/pages/Admin/Auth/Login/index.tsx
@@ -1,6 +1,6 @@
 import './styles.css';
 
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import ButtonIcon from 'components/ButonIcon';
 import { useForm } from "react-hook-form"
 import { getTokenData, requestBackendLogin, saveAuthData } from 'util/requests';
@@ -12,7 +12,16 @@ type FormData = {
     password: string;
 }
 
+type LocationState = {
+    from: string;
+}
+
 const Login = () => {
+
+    const location = useLocation<LocationState>();
+
+    const {from} = location.state || {from: {pathname: '/admin'}};
+
     const { setAuthContextData } = useContext(AuthContext);
 
     const [hasError, setHasError] = useState(false);
@@ -30,7 +39,7 @@ const Login = () => {
                     authenticate: true,
                     tokenData: getTokenData()
                 })
-                history.push('/admin');
+                history.replace(from);
             })
             .catch(error => {
                 setHasError(true);
