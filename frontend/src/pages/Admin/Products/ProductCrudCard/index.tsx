21178a8102d24f34e45b68c0b763b77301e8229e
João Paulo <jp_cbc@hotmail.com>
2024-02-22 01:17:42 -0300

    Configurando rotas do CRUD e mudando imagem de produtos

--- frontend/src/pages/Admin/Products/ProductCrudCard/index.tsx
+++ frontend/src/pages/Admin/Products/ProductCrudCard/index.tsx
@@ -2,6 +2,7 @@ import './styles.css'
 import ProductPrice from 'components/ProductPrice';
 import { Product } from 'types/product';
 import CategoryBadge from '../CategoryBadge';
+import { Link } from 'react-router-dom';
 
 type Props = {
     product: Product;
@@ -31,10 +32,12 @@ const ProductCrudCard = ({ product }: Props) => {
                 <button className='btn btn-outline-danger product-crud-card-button product-crud-card-button-first'>
                     excluir
                 </button>
-                <button className='btn btn-outline-secondary product-crud-card-button'>
-                    editar
-                </button>
-            </div>                
+                <Link to={`/admin/product/${product.id}`}>
+                    <button className='btn btn-outline-secondary product-crud-card-button'>
+                        editar
+                    </button>
+                </Link>
+            </div>
         </div>
     );
 }
